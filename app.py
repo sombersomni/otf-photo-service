410c7c02ba89a6e4f3ffb162db55d0a581a8726c
somberSomni <kartunehustla@gmail.com>
2023-03-04 22:59:05 -0500

    add cors support for frontend calls

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask
+from flask_cors import CORS
 
 from routes.api.blueprint import api_bp
 
 from middleware.session import session_middleware
 
 app = Flask(__name__)
-
+# register routes
 app.register_blueprint(api_bp, url_prefix='/api')
+# register middleware
 session_middleware(app)
+cors = CORS(app)
+cors = CORS(app, resources={
+    r"/*": {
+        "origins": "http://localhost:3000"
+    }
+})
 
 @app.route('/')
 def index():
